232c1659adafbe1872d5e127cf8063841b7630c7
Chalid Bahfen <bahfenchalid@gmail.com>
2021-09-10 10:48:44 +0700

    day05 preview done

--- day04/Latihan.js
+++ day04/Latihan.js
@@ -1,21 +1,80 @@
 import Product from "./product.js";
 
-const prod1 = new Product(1,"Samsung A1","HP",2500000,3,7500000);
-const prod2 = new Product(2,"Dell Ultrabook","COMPUTER",7850000,2,15700000);
+const prod1 = new Product(1,"Samsung A1","HP",2500000,3);
+const prod2 = new Product(2,"Dell Ultrabook","COMPUTER",7850000,2);
 const prod3 = new Product(3,"Mesin Cuci LG","ELECTRONIC",3500000);
-const prod4 = new Product(4,"Iphone 12","HP",6000000,2,1200000);
-const prod5 = new Product(5,"Asus Laptop","COMPUTER",6500000,4,2600000);
+const prod4 = new Product(4,"Iphone 12","HP",6000000,2);
+const prod5 = new Product(5,"Asus Laptop","COMPUTER",6500000,4);
 
-const listProduct = [prod1,prod2,prod3,prod4,prod5];
+// prod1.setPrice(1000);
+// prod1.setPriceJmlBeli(200,20);
+
+let listProduct = [];
+
+//add product 
+function addToProduct(product) {
+    listProduct = [...listProduct,product];
+    return listProduct;
+}
+
+addToProduct(prod1);
+addToProduct(prod2);
+addToProduct(prod3);
+addToProduct(prod4);
+addToProduct(prod5);
+
+//display all product
+function findAllProduct(array=[]) {
+    return array;
+}
+console.log(findAllProduct(listProduct));
+
+//anonymous function
+(hello)=>{
+    console.log(hello);
+}
+
+//filter by category
+function listProductByCategory(category) {
+    return listProduct.filter((el) => el.category === category);
+}
+console.log(listProductByCategory("HP"));
+console.log(listProductByCategory("COMPUTER"));
+console.log(listProductByCategory("ELECTRONIC"));
+
+//hitung total
+const totalProduct = listProduct.reduce((sum,jmlHarga) => sum + jmlHarga.subTotal,0);
+console.log(`Total = Rp.${totalProduct}`);
+
+//hitung total + discount
+const subTotalDiscount = listProduct.reduce((sum,val) =>{
+    if(val.jumlahBeli > 1 ){
+        return sum + (val.subTotal - (val.subTotal * 10/100));
+    }else{
+        return sum + val.subTotal;
+    }
+},0)
+console.log(`Total tagihan + discount = Rp. ${subTotalDiscount}`);
+
+//hitung total + discount with ternary
+const subTotalDiscount2 = listProduct.reduce((sum,val) =>
+(val.jumlahBeli > 1) ? sum + (val.subTotal - (val.subTotal * 10/100)) : (sum + val.subTotal),0);
+
+console.log(`Total tagihan + discount = Rp. ${subTotalDiscount2}`);
+
+
 /*
+listProduct = [...listProduct,prod1]
+listProduct = [...listProduct,prod2]
+listProduct = [...listProduct,prod3]
+listProduct = [...listProduct,prod4]
+listProduct = [...listProduct,prod5]
+*/
+
 console.log(prod1.toString());
 console.log(prod2.toString());
 console.log(prod3.toString());
 console.log(prod4.toString());
 console.log(prod5.toString());
-*/
-
-const totalProduct = listProduct.reduce((sum,jmlHarga) => sum + jmlHarga.subTotal,0);
-console.log(`Total = Rp.${totalProduct}`);
 
 console.log();
--- day04/product.js
+++ day04/product.js
@@ -1,15 +1,27 @@
 export default class Product{
-    constructor(prodId,prodName,category="ELECTRONIC",price,jumlahBeli = 1,subTotal){
+    constructor(prodId,prodName,category,price=0,jumlahBeli = 1){
         this.prodId = prodId;
         this.prodName = prodName;
         this.category = category;
         this.price = price;
         this.jumlahBeli = jumlahBeli;
-        this.subTotal = subTotal;
+        this.subTotal = this.price * this.jumlahBeli;
+    }
+
+    setPrice(price){
+        this.price = price;
+        return this.subTotal = this.price * this.jumlahBeli;
+    }
+
+    setPriceJmlBeli(price,jumlahBeli){
+        this.price = price;
+        this.jumlahBeli = jumlahBeli;
+        return this.subTotal = this.price * this.jumlahBeli;
     }
 
     toString(){
-        return `${this.prodId}. ${this.prodName}, category : ${this.category}, Harga : ${this.price}, Qty : ${this.jumlahBeli}, Subtotal = ${this.price * this.jumlahBeli}`
+        return `${this.prodId}. ${this.prodName}, category : ${this.category}, 
+        Harga : ${this.price}, Qty : ${this.jumlahBeli}, Subtotal : ${this.subTotal}`
     }
 
 
